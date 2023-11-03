afe3f9a2469ce60f63dbcb61c5348e39a840d0fc
tssvett <tssvett@mail.ru>
2023-11-03 20:56:30 +0300

    Now the Logger Class can store information from different loggers in different files.

--- src/back/LoggerClass.py
+++ src/back/LoggerClass.py
@@ -5,18 +5,23 @@ import os
 
 class Logger:
     """
-    Logger class. Logs messages to file and console.
+    Logger class. Logs message to file and console.
+
+    Use the same names for loggers if you need to store information in one file (with the name of the logger)
+
+    Use different names for loggers if you need to store information separately from each other (in different files)
+
     """
-    def __init__(self, name):
+    def __init__(self, name: str):
         """
         Constructor.
-        :param name: Need to be unique.
         """
         self._log_core = logging.getLogger(name)
         self._log_core.setLevel(level=logging.DEBUG)
+        self._logs_path = os.path.join(os.path.dirname(__file__)[:-5], 'Logs', f'{name}.log')
         if self._log_core.hasHandlers():
             self._log_core.handlers.clear()
-        self._file_handler = logging.FileHandler(os.path.dirname(__file__)[:-5] + r'\TokenFiles\info.log')
+        self._file_handler = logging.FileHandler(self._logs_path)
         self._console_handler = logging.StreamHandler(stream=sys.stdout)
         self._set_format()
         self._add_all_handlers()
@@ -40,33 +45,29 @@ class Logger:
         self._file_handler.setFormatter(file_format)
         self._console_handler.setFormatter(console_format)
 
-    def send_exception_message(self, message: str) -> str:
+    def send_exception_message(self, message: str) -> None:
         """
         Sends exception message to file by logger.
         """
         self._log_core.exception(message)
-        return message
 
-    def send_info_message(self, message: str) -> str:
+    def send_info_message(self, message: str) -> None:
         """
         Sends information message to file by logger.
         """
         self._log_core.info(message)
-        return message
 
-    def send_error_message(self, message: str) -> str:
+    def send_error_message(self, message: str) -> None:
         """
         Sends critical error message to file by logger.
         """
         self._log_core.error(message)
-        return message
 
-    def send_debug_message(self, message: str) -> str:
+    def send_debug_message(self, message: str) -> None:
         """
         Sends debug message to file by logger.
         """
         self._log_core.debug(message)
-        return message
 
     def get_logger_object(self):
         """
@@ -74,3 +75,16 @@ class Logger:
         :return:
         """
         return self._log_core
+
+    def get_logs_path(self):
+        """
+        Returns logs path
+        """
+        return self._logs_path
+
+    def set_logs_path(self, path):
+        """
+        Sets logs path
+        """
+        self._logs_path = path
+
